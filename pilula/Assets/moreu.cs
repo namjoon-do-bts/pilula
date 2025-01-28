c9a3141f4c3ca939b8ae8119c38bbb9692f9b592
namjoon-do-bts <flor.l@escolar.ifrn.edu.br>
2025-01-28 17:50:18 -0300

    pilula e moeda

--- pilula/Assets/moreu.cs
+++ pilula/Assets/moreu.cs
@@ -1,9 +1,38 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class moreu : MonoBehaviour
 {
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Moreu : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
